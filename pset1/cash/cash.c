f20cae218e399c0c5ca19d40afcb4edd132aafbb
Bhaskar Joshi <56031616+BhaskarJoshi-01@users.noreply.github.com>
2020-02-27 23:10:59 +0530

    P_set_1 codes

---
+++ pset1/cash/cash.c
@@ -0,0 +1,54 @@
+#include <cs50.h>
+#include <math.h>
+#include <stdio.h>
+
+int main()
+{
+
+    int coin = 0;
+    // Prompt user for an amount of change
+    float change;
+    do
+    {
+        change = get_float("Change owed: ");
+    }
+    while (change < 0);
+
+
+    
+
+    // Convert dollars to cents
+    int amount = (int)round(change * 100);
+
+     // Check how many pennies fit in change
+    if (amount % 1 != amount)
+    {
+        coin += amount / 1;
+        amount = amount % 1;
+    }
+
+    // Check how many nickels fit in change
+    if (amount % 5 != amount)
+    {
+        coin += amount / 5;
+        amount = amount % 5;
+
+    }
+   // Check how many dimes fit in change
+    if (amount % 10 != amount)
+    {
+        coin += amount / 10;
+        amount = amount % 10;
+
+    }
+   // Check how many quarters fit in change
+    if (amount % 25 != amount)
+    {
+        coin += amount / 25;
+        amount = amount % 25;
+
+    }
+
+ 
+    printf("%d\n", coin);
+}
